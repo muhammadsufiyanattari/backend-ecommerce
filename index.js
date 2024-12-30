5f6b871760cfe7fd0203c406ee3e09bf364726f5
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-31 01:42:00 +0500

    changing

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 import cors from 'cors'
-import { products } from './src/products.js'
-
+// import { products } from './src/products.js'
+ const products=[]
 app.use(cors())
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/products', (req, res) => {
+    res.send(products)
+  })
+  app.post('/products/post', (req, res) => {
+    console.log('POST /products' ,req.body);
+  products.push(req.body)
     res.send(products)
   })
 
